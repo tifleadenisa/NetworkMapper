214fcfb67a2ac6e23df0dc7dca1d57300f7dab72
Denisa Ionela <denisa.tiflea@info.uaic.ro>
2021-12-12 23:55:27 +0200

    Final version

--- NetworkMapper/main.py
+++ NetworkMapper/main.py
@@ -2,81 +2,47 @@ import ipaddress
 import sys
 import nmap
 import socket
-import time
+
+
+def testing_ports(addresses, ports):
+    ports_names = {80: "HTTP", 443: "HTTPS", 21: "FTP", 22: "FTPS/SSH", 110: "POP3", 995: "POP3 SSL",
+                   143: "IMAP", 993: "IMAP SSL", 25: "SMTP", 587: "SMTP SSL", 23: "Telnet", 50: "IPSec",
+                   51: "IPSec", 53: "DNS", 3389: "Remote Desktop Protocol", 445: "SMB"}
+
+    found_results = False
+    for ipaddr in addresses:
+        for port in ports:
+            a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            location = (ipaddr, int(port))
+            if a_socket.connect_ex(location) == 0:
+                found_results = True
+                if int(port) in ports_names:
+                    print(location[0] + ":" + str(location[1]) + " (" + ports_names.get(int(port)) + ")")
+                else:
+                    print(location[0] + ":" + str(location[1]))
+            a_socket.close()
+    if not found_results:
+        print("The mentioned ports are not open for the given addresses")
+
 
 if __name__ == '__main__':
 
-    print(sys.argv)
     network = ipaddress.ip_network(sys.argv[1])
-    print(network)
-    print(network.num_addresses)
+    port_parameters = []
+    for i in range(2, len(sys.argv)):
+        port_parameters.append(sys.argv[i])
 
-    print("Now we check if an addres is up or not")
-
-    up_addr = []
-
-    t1 = time.time()
-    for ip_addr in network.hosts():
+    up_addresses = []
+    for ip_address in network.hosts():
         scanner = nmap.PortScanner()
-        host = socket.gethostbyname(str(ip_addr))
+        host = socket.gethostbyname(str(ip_address))
         scanner.scan(host, '1', '-v')
-        print("IP Status for ", ip_addr, " is ", scanner[host].state())
         if scanner[host].state() == "up":
-            up_addr.append(str(ip_addr))
+            up_addresses.append(str(ip_address))
 
-    print("--------------")
-    t2 = time.time()
-    print("i have tested connections for " + str((t2-t1) / 60.0) + " minutes")
-    print("--------------")
-    print("the following addresses are up: ")
-    for x in up_addr:
-        print(x)
-
-    up_addr1 = ["192.168.2.1", "192.168.2.110", "192.168.2.197"]
-
-    print("Testing the most used ports(80, 443, 445, 21, 22, 110, 995, 143, 993, 25, 587) for the above addresses")
-
-    a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    for x in up_addr:
-
-        location = (x, 80)
-        if a_socket.connect_ex(location) == 0:
-            print(location[0] + ":" + str(location[1]) + " (HTTP)")
-        location = (x, 443)
-        if a_socket.connect_ex(location) == 0:
-            print(location[0] + ":" + str(location[1]) + " (HTTPS)")
-        location = (x, 445)
-        if a_socket.connect_ex(location) == 0:
-            print(location[0] + ":" + str(location[1]))
-        location = (x, 21)
-        if a_socket.connect_ex(location) == 0:
-            print(location[0] + ":" + str(location[1]) + " (FTP)")
-        location = (x, 22)
-        if a_socket.connect_ex(location) == 0:
-            print(location[0] + ":" + str(location[1]) + " (FTPS/SSH)")
-        location = (x, 110)
-        if a_socket.connect_ex(location) == 0:
-            print(location[0] + ":" + str(location[1]) + " (POP3)")
-        location = (x, 995)
-        if a_socket.connect_ex(location) == 0:
-            print(location[0] + ":" + str(location[1]) + " (POP3 SSL)")
-        location = (x, 143)
-        if a_socket.connect_ex(location) == 0:
-            print(location[0] + ":" + str(location[1]) + " (IMAP)")
-        location = (x, 993)
-        if a_socket.connect_ex(location) == 0:
-            print(location[0] + ":" + str(location[1]) + " (IMAP SSL)")
-        location = (x, 25)
-        if a_socket.connect_ex(location) == 0:
-            print(location[0] + ":" + str(location[1]) + " (SMTP)")
-        location = (x, 587)
-        if a_socket.connect_ex(location) == 0:
-            print(location[0] + " " + str(location[1]) + " (SMTP SSL)")
-
-        # print(x)
-        # if a_socket.connect_ex(location) == 0:
-        #     print("port is open")
-        # else:
-        #     print("port is not open")
-
-
+    if len(sys.argv) > 2:
+        print("Testing the ports that were given as parameters for the above addresses")
+        testing_ports(up_addresses, port_parameters)
+    else:
+        print("Testing the most used ports(80, 443, 445, 21, 22, 110, 995, 143, 993, 25, 587) for the above addresses")
+        testing_ports(up_addresses, [80, 443, 445, 21, 22, 110, 995, 143, 993, 25, 587])
